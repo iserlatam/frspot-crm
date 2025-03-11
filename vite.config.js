cea7759badd8eb494b9f4666b6a9138bd1dec758
iserlatam <iserlatam@gmail.com>
2025-03-10 19:48:47 -0500

    2FA Agregado para Extension de navegador 'authenticator'

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/client/theme.css',],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/client/theme.css",
+                "./vendor/vormkracht10/filament-2fa/resources/**.*.blade.php",
             ],
+            refresh: [...refreshPaths, "app/Livewire/**"],
         }),
     ],
 });
