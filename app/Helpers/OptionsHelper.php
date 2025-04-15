9698263fbcceb356a6722f1d10646544558102bd
Iser Solutions <iserlatam@gmail.com>
2025-04-15 18:02:05 -0500

    Avena de camello

--- app/Helpers/OptionsHelper.php
+++ app/Helpers/OptionsHelper.php
@@ -1,33 +1,37 @@
 <?php
+
 namespace App\Helpers;
 
 class OptionsHelper
 {
-    public static function estadoOptions():array
+    public static function estadoOptions(): array
     {
         return self::getOptions('estado_cliente');
     }
-    public static function faseOptions():array
+    public static function faseOptions(): array
     {
         return self::getOptions('fase_cliente');
     }
-    public static function origenOptions():array
+    public static function origenOptions(): array
     {
         return self::getOptions('origenes');
     }
 
-    public static function getOptions(string $campo):array
+    public static function getOptions(string $campo): array
     {
         $path = app_path('Helpers\opciones_cliente.json');
+        // dd($path);
         if (!file_exists($path)) {
-            return [];
+            return json_decode(file_get_contents($path), true);
         }
         $data =  json_decode(file_get_contents($path), true);
 
-        
-
         return collect($data[$campo] ?? [])
-            ->mapWithKeys(fn($item) => [$item => $item])
+            ->mapWithKeys(
+                function ($item) {
+                    return $item;
+                }
+            )
             ->toArray();
     }
 
@@ -35,10 +39,10 @@ class OptionsHelper
     {
         $path = app_path('Helpers\opciones_cliente.json');
         $data = file_exists($path)
-        ? json_decode(file_get_contents($path), true)
-        : [];
+            ? json_decode(file_get_contents($path), true)
+            : [];
 
-        if(!in_array($valor, $data[$campo] ?? [])){
+        if (!in_array($valor, $data[$campo] ?? [])) {
             $data[$campo][] = $valor;
             file_put_contents($path, json_encode($data, JSON_PRETTY_PRINT));
         }
