0ed3a4798f16f28d145c431cd8e0654c2ee7ce2a
Xenower <1025761182alan@gmail.com>
2025-04-30 14:48:49 -0500

    ajuste 2

--- resources/views/filament/client/pages/auth/login-extra.blade.php
+++ resources/views/filament/client/pages/auth/login-extra.blade.php
@@ -1,19 +1,19 @@
 {{-- resources/views/filament/login-extra.blade.php --}}
 @vite('resources/css/filament/client/login.css')
 
-<header class="bg-black text-white text-lg max-h-14 px-6 flex items-center">
+<header class="bg-black text-white max-h-14 px-6 flex items-center">
     <a href="https://frspot.com/" class="mr-4">
         <img src="{{ asset('login-imgs/logo.jpeg') }}" alt="Logo" style="height: 80px">
     </a>
-    <h1 class="text-xl font-semibold">
-        Bienvenido 
+    <h1 class="text-xl font-semibold uppercase ">
+        Bienvenido
 
         @php
             $user = filament()->auth()->user();
         @endphp
 
         @if ($user)
-            <span class="ml-2 text-lg text-gray-300 uppercase">{{ $user->name }}</span>
+            <span class="ml-2 text-xl text-gray-300 uppercase">{{ $user->name }}</span>
         @endif
     </h1>
 </header>
