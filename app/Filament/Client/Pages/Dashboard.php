91f839294495b4722a839ba0aae7beb6d2ff5d93
Iser Solutions <iserlatam@gmail.com>
2025-05-03 17:25:04 -0500

    Añadido icono y widget de stats al inicio del cliente

--- app/Filament/Client/Pages/Dashboard.php
+++ app/Filament/Client/Pages/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Client\Pages;
 
+use App\Filament\Client\Widgets\AccountClientInfo;
 use Filament\Pages\Page;
 
 class Dashboard extends Page
@@ -9,4 +10,13 @@ class Dashboard extends Page
     protected static string $routePath = '/';
 
     protected static string $view = 'filament.client.pages.dashboard-page';
+
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            AccountClientInfo::class,
+        ];
+    }
 }
