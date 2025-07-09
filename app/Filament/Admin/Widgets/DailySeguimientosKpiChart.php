920fd814112bee9f9070951873ac1c36a79b8ec2
Xenower <1025761182alan@gmail.com>
2025-07-09 15:43:25 -0500

    ajuste solo roles asesores kpi para la grafica

--- app/Filament/Admin/Widgets/DailySeguimientosKpiChart.php
+++ app/Filament/Admin/Widgets/DailySeguimientosKpiChart.php
@@ -43,6 +43,7 @@ class DailySeguimientosKpiChart extends ChartWidget
                 (aunque no tengan actividad, saldrán con valor 0) */
         $asesores = Asesor::query()
             ->whereIn('tipo_asesor', ['ftd', 'retencion', 'recovery'])
+            ->whereIn('user.roles.name',['asesor'])
             ->with('user:id,name')     // para obtener el nombre a mostrar
             ->get();
 
